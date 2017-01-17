74a4cd2a92de1dc2cb3b4a5608ed319a8bfe70b5
Ognjen Tomic <gliamanti@gmail.com>
2017-01-17 16:02:15 +0100

    PB-16 send gps data to att platform complete

--- main/gps.ino
+++ main/gps.ino
@@ -1,43 +1,6 @@
-#include <LGPS.h>
-
-  /* Refer to http://www.gpsinformation.org/dale/nmea.htm#GGA
-   * Sample data: $GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47
-   * Where:
-   *  GGA          Global Positioning System Fix Data
-   *  123519       Fix taken at 12:35:19 UTC
-   *  4807.038,N   Latitude 48 deg 07.038' N
-   *  01131.000,E  Longitude 11 deg 31.000' E
-   *  1            Fix quality: 0 = invalid
-   *                            1 = GPS fix (SPS)
-   *                            2 = DGPS fix
-   *                            3 = PPS fix
-   *                            4 = Real Time Kinematic
-   *                            5 = Float RTK
-   *                            6 = estimated (dead reckoning) (2.3 feature)
-   *                            7 = Manual input mode
-   *                            8 = Simulation mode
-   *  08           Number of satellites being tracked
-   *  0.9          Horizontal dilution of position
-   *  545.4,M      Altitude, Meters, above mean sea level
-   *  46.9,M       Height of geoid (mean sea level) above WGS84
-   *                   ellipsoid
-   *  (empty field) time in seconds since last DGPS update
-   *  (empty field) DGPS station ID number
-   *  *47          the checksum data, always begins with *
-   */
-
-gpsSentenceInfoStruct info;
 char buff[256];
-struct Locations location;
 
-void initializeLocation(struct Locations loc) {
-  loc.satellite_status = 0;
-  loc.latitude = 0.0;
-  loc.longitude = 0.0;
-  sprintf(buff, " satellite_status: %d\n latitude: %f\n longitude: %f\n", loc.satellite_status, loc.latitude, loc.longitude);
-  Serial.println(buff);
-};
-
+//////////////////////////// HELPER FUNCTIONS //////////////////////////////////
 static unsigned char getComma(unsigned char num,const char *str)
 {
   unsigned char i,j = 0;
@@ -46,6 +9,7 @@ static unsigned char getComma(unsigned char num,const char *str)
   {
      if(str[i] == ',')
       j++;
+     
      if(j == num)
       return i + 1; 
   }
@@ -63,6 +27,7 @@ static double getDoubleNumber(const char *s)
   strncpy(buf, s, i);
   buf[i] = 0;
   rev=atof(buf);
+  
   return rev; 
 }
 
@@ -77,8 +42,19 @@ static double getIntNumber(const char *s)
   strncpy(buf, s, i);
   buf[i] = 0;
   rev=atoi(buf);
+  
   return rev; 
 }
+
+void initializeLocation(struct Locations loc) 
+{
+  loc.satellite_status = 0;
+  loc.latitude = 0.0;
+  loc.longitude = 0.0;
+  
+  sprintf(buff, " satellite_status: %d\n latitude: %f\n longitude: %f\n", loc.satellite_status, loc.latitude, loc.longitude);
+  Serial.println(buff);
+};
 
 struct Locations parseGPGGA(const char* GPGGAstr)
 {
@@ -93,8 +69,10 @@ struct Locations parseGPGGA(const char* GPGGAstr)
     {
       tmp = getComma(2, GPGGAstr);
       loc.latitude = getDoubleNumber(&GPGGAstr[tmp]) * 0.01;
+      
       tmp = getComma(4, GPGGAstr);
       loc.longitude = getDoubleNumber(&GPGGAstr[tmp]) * 0.01;
+      
       sprintf(buff, "latitude = %f\nlongitude = %f", loc.latitude, loc.longitude);
       Serial.println(buff);
 
@@ -112,27 +90,37 @@ struct Locations parseGPGGA(const char* GPGGAstr)
     Serial.println("Not get data"); 
   }
 }
+////////////////////////////////////////////////////////////////////////////////////////////////
 
-void initializeDevice() {
+
+void initializeGps()                                     // this method is first to be called during setup process 
+{
   Serial.println("\n====== GPS initialization ======");
-  initializeLocation(location);
+  
+  initializeLocation(tmpLocation);
+  
   LGPS.powerOn();
+  
   Serial.println("LGPS Power on, and waiting ..."); 
+  
   delay(3000);
 }
 
-struct Locations getGpsData() {
+struct Locations getGpsData()                           // use this method to acquire gps cordinates after initialization has been made
+{
   LGPS.getData(&info);
-  Serial.println((char*)info.GPGGA); 
-  location = parseGPGGA((const char*)info.GPGGA);
   
-  if(location.satellite_status == 0)
+  Serial.println("RAW data: ");
+  Serial.println((char*)info.GPGGA); 
+  
+  tmpLocation = parseGPGGA((const char*)info.GPGGA);
+  
+  if(tmpLocation.satellite_status == 0)
   {   
-       delay(1000);
        return getGpsData();
   }
   else
   {
-    return location;
+    return tmpLocation;
   };
 }
--- main/main.ino
+++ main/main.ino
@@ -1,86 +1,78 @@
-/*
-   Copyright 2014-2016 AllThingsTalk
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-       http://www.apache.org/licenses/LICENSE-2.0
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <LGPS.h>
 #include <LGPRS.h>
 #include <LGPRSClient.h>
 #include <LGPRSServer.h>
 #include <LGPRSUdp.h>
-//#include <LTask.h>
 #include <PubSubClient.h>
 #include <ATT_IOT.h>
 
+#define httpServer "api.AllThingsTalk.io"                  
+#define mqttServer httpServer 
 char deviceId[] = "tIfnBDpabrH4K6aF5l2RCAnv";
 char clientId[] = "ognjent_EjGwg4VW";
 char clientKey[] = "GsqcBmey";
+ATTDevice Device(deviceId, clientId, clientKey);
+LGPRSClient c;
+int id = 2;
 
-int a0=A0;
-int a0Id=0;
-int a1=8;
-int a1Id=1;
-int a2=1;
-int a2Id=2;
+///////////////// GPS_VARIABLES /////////////////////////////////
+gpsSentenceInfoStruct info;
 struct Locations {
   int satellite_status;
   double latitude;
   double longitude;
 };
+struct Locations tmpLocation;
 struct Locations gpsData;
-ATTDevice Device(deviceId, clientId, clientKey);                //create the object that provides the connection to the cloud to manager the device.
-#define httpServer "api.AllThingsTalk.io"                  // HTTP API Server host                  
-#define mqttServer httpServer                               // MQTT Server Address 
-//required for the device
+/////////////////////////////////////////////////////////////////
+
 void callback(char* topic, byte* payload, unsigned int length);
-LGPRSClient c;
+
 PubSubClient pubSub(mqttServer, 1883, callback, c);
 
 void setup()
 {
-  initializeDevice();
   Serial.begin(115200);
-  while(!Serial);                                                       //for the linkit, we need to wait until the serial monitor is initialized correctly, if we don't do this, we don't get to see the logging.
-                                                                        //Warning for battery and mains power usage, comment out the above line, otherwise the setup() will continue to wait for the Serial to become available
-  Serial.println("starting");
-  pinMode(a1, OUTPUT);
+  while(!Serial);
   
+  initializeGps();
+  Serial.println("starting");
+
   while(!LGPRS.attachGPRS("NET","telenor","gprs"))
   {
     Serial.println("connecting");
     delay(1000);
   }
   Serial.println("connected");
-  while(!Device.Connect(&c, httpServer))                        // connect the device with the IOT platform.
+  
+  while(!Device.Connect(&c, httpServer))
     Serial.println("retrying");
-  Device.AddAsset(a0Id, "knob", "rotary switch",false, "integer");
-  Device.AddAsset(a1Id, "led", "light emitting diode", true, "boolean");
-  Device.AddAsset(a2Id, "gps", "gps sensor", false, "string");
-  while(!Device.Subscribe(pubSub))                                      // make certain that we can receive message from the iot platform (activate mqtt)
+  
+  Device.AddAsset(id, "GPS", "Global Positioning System", false, "string");
+  
+  while(!Device.Subscribe(pubSub))
     Serial.println("retrying"); 
-  digitalWrite(a1, LOW);
 }
+
 unsigned long time;
 void loop()
 {
   unsigned long curTime = millis();
-  if (curTime > (time + 20000))                                          // publish light reading every 5 seconds to sensor 1
+  if (curTime > (time + 20000))
   {
     gpsData = getGpsData();
+    
     int length = 40;
-    char* cords = new char[length];
-    sprintf(cords, "%f,%f",gpsData.latitude, gpsData.longitude);
-    Device.Send(String(cords), a2Id);
+    char* coords = new char[length];
+    sprintf(coords, "%f,%f",gpsData.latitude, gpsData.longitude);
+    
+    Device.Send(String(coords), id);
     
     time = curTime; 
   };
   Device.Process(); 
 }
+
 // Callback function: handles messages that were sent from the iot platform to this device.
 void callback(char* topic, byte* payload, unsigned int length) 
 { 
@@ -101,22 +93,6 @@ void callback(char* topic, byte* payload, unsigned int length)
     Serial.println(msgString);
     Serial.print("topic: ");
     Serial.println(topic);
-    
-    if (pinNr == a1Id)
-    {
-      if (msgString == "false") {
-        Serial.println("LED off");  
-            digitalWrite(a1, LOW);           //change the LED status to false
-            idOut = &a1Id;
-                              
-      }
-      else if (msgString == "true") {
-        Serial.println("LED on"); 
-        digitalWrite(a1, HIGH);              //change the LED status to true
-            idOut = &a1Id;
-           
-      }
-    }
   }
   if(idOut != NULL)                //also let the iot platform know that the operation was succesful: give it some feedback. This also allows the iot to update the GUI's correctly & run scenarios.
     Device.Send(msgString, *idOut);    
