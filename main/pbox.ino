465d7dbc582d3b21ebf6afcc2d35003e6c86ebef
Ognjen Tomic <gliamanti@gmail.com>
2017-02-24 09:27:39 +0100

    create box withing pbox server imeplemetned

--- main/pbox.ino
+++ main/pbox.ino
@@ -9,81 +9,70 @@ void createBox(String boxName,char* deviceId) {
   } 
  
   String data = "{\"code\": \""+ boxName +"\", \"deviceId\": \""+ deviceId +"\"}";
-  //String thisLength = String(data.length());
+  String thisLength = String(data.length());
 
   Serial.println("Creating BOX in system ...");
-  
-//  // Build HTTP POST request
-//  c.print("POST /v1.0/boxes");
-//  c.println(" HTTP/1.1");
-//  c.print("Host: ");
-//  c.println(PBOX_SERVER);
-//  c.println("Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdXRoVXNlcklkIjoiNTg5ZDhlZDE2ZjExZDQwMDA0NmQzZTUzIn0.h58hw0TivKLIaWlvG8cUlxxezduNu-QZG8XbgX4wjwY");
-//  c.println("Content-Type: application/json");
-// // c.println("Content-Length: " + thisLength);
-//  c.println("Cache-Control: no-cache");
-//  c.print("\n" + data);
-//  c.print(char(26));
-
-
 
     // send HTTP request, ends with 2 CR/LF
-    Serial.print("POST /v1.0/boxes");
-    Serial.println(" HTTP/1.1");
-    Serial.print("Host: ");
-    Serial.println(PBOX_SERVER);
+    Serial.println("POST /v1.0/boxes HTTP/1.1");
+    Serial.println("Host: pbox-test.herokuapp.com");
     Serial.println("Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdXRoVXNlcklkIjoiNTg5ZDhlZDE2ZjExZDQwMDA0NmQzZTUzIn0.h58hw0TivKLIaWlvG8cUlxxezduNu-QZG8XbgX4wjwY");
+    Serial.println("User-Agent: Linkit/1.0");
     Serial.println("Content-Type: application/json");
-    Serial.print("\n" + data);
-    Serial.print(char(26));
+    Serial.println("Accept: application/json");
+    Serial.println("Content-Length: " + thisLength);
+
     Serial.println("Connection: close");
     Serial.println();
-    c.print("POST /v1.0/boxes");
-    c.println(" HTTP/1.1");
-    c.print("Host: ");
-    c.println(PBOX_SERVER);
+    Serial.println(data);
+    
+    c.println("POST /v1.0/boxes HTTP/1.1");
+    c.println("Host: pbox-test.herokuapp.com");
     c.println("Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdXRoVXNlcklkIjoiNTg5ZDhlZDE2ZjExZDQwMDA0NmQzZTUzIn0.h58hw0TivKLIaWlvG8cUlxxezduNu-QZG8XbgX4wjwY");
+    c.println("User-Agent: Linkit/1.0");
     c.println("Content-Type: application/json");
-    c.print("\n" + data);
-    c.print(char(26));
+    c.println("Accept: application/json");
+    c.println("Content-Length: " + thisLength);
+
     c.println("Connection: close");
     c.println();
-
-    while (c){
-      Serial.print((char)c.read());
-    }
+    c.println(data);
+//
+//    while (c){
+//      Serial.print((char)c.read());
+//    }
   
 ////    // waiting for server response
-//    Serial.println("waiting HTTP response:");
-//    while (!c.available())
-//    {
-//      delay(100);
-//    }
-//
-//    boolean disconnectedMsg = false;
-//
-//    while (c)
-//    {
-//      int v = c.read();
-//      if (v != -1)
-//      {
-//        Serial.print((char)v);
-//      }
-//      else
-//      {
-//        Serial.println("no more content, disconnect");
-//        c.stop();
-//        while (1)
-//        {
-//          delay(1);
-//        }
-//      }
-//    }
-//
-//    if (!disconnectedMsg)
-//    {
-//      Serial.println("disconnected by server");
-//      disconnectedMsg = true;
-//    }
-//    delay(500);
+    Serial.println("waiting HTTP response:");
+    while (!c.available())
+    {
+      delay(100);
+    }
+
+    boolean disconnectedMsg = false;
+
+    while (c)
+    {
+      int v = c.read();
+      if (v != -1)
+      {
+        Serial.print((char)v);
+      }
+      else
+      {
+        Serial.println("no more content, disconnect");
+        c.stop();
+        while (1)
+        {
+          delay(1);
+        }
+      }
+    }
+
+    if (!disconnectedMsg)
+    {
+      Serial.println("disconnected by server");
+      disconnectedMsg = true;
+    }
+    delay(500);
 }
